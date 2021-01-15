c3dd3533e43753cae96ecef853f761c55bbfbb42
Antrems <voropaefftema@yandex.ru>
2021-01-15 12:46:26 +0300

    Merge create-tasks-array to main

--- index.js
+++ index.js
@@ -10,17 +10,39 @@ const tasksList = [
 	{ id: "7", text: "получить работу", completed: false }
 ];
 
+<<<<<<< HEAD
 const todoList = document.querySelector('.todo-list');
 const listItemTemplate = document.querySelector('#list-item-template').content.querySelector('li');
 
 // Todo-list elements
+=======
+const tasksList = [
+	{ id: "1", text: "выучить html", completed: true },
+	{ id: "2", text: "выучить css", completed: true },
+	{ id: "3", text: "выучить js", completed: false },
+	{ id: "4", text: "выучить фреймворк", completed: false },
+	{ id: "5", text: "написать несколько учебных проектов", completed: false },
+	{ id: "6", text: "пройти собеседование", completed: false },
+	{ id: "7", text: "получить работу", completed: false }
+];
+
+const todoList = document.querySelector('.todo-list');
+const listItemTemplate = document.querySelector('#list-item-template').content.querySelector('li');
+
+
+>>>>>>> baa6e84... Merge
 listItemTemplate.querySelector('div').classList.add('view');
 listItemTemplate.querySelector('input').classList.add('toggle');
 listItemTemplate.querySelector('input').setAttribute('type', 'chekbox');
 listItemTemplate.querySelector('button').classList.add('destroy');
 
+<<<<<<< HEAD
 // Add information from Enter data to List item Templete 
 let createListItem = function (tasks) {
+=======
+
+let createListItem = function (task) {
+>>>>>>> baa6e84... Merge
 
 	let listItem = listItemTemplate.cloneNode(true);
 
