bbf1d9b2ea922088de36bfb81a94c62f08869051
Antrems <voropaefftema@yandex.ru>
2021-01-15 12:36:38 +0300

    Merge

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 'use strict';
-// Enter data
-const tasksList = [
-	{ id: "1", text: "выучить html", completed: true },
-	{ id: "2", text: "выучить css", completed: true },
-	{ id: "3", text: "выучить js", completed: false },
-	{ id: "4", text: "выучить фреймворк", completed: false },
-	{ id: "5", text: "написать несколько учебных проектов", completed: false },
-	{ id: "6", text: "пройти собеседование", completed: false },
-	{ id: "7", text: "получить работу", completed: false }
-];
 
-<<<<<<< HEAD
-const todoList = document.querySelector('.todo-list');
-const listItemTemplate = document.querySelector('#list-item-template').content.querySelector('li');
-
-// Todo-list elements
-=======
 const tasksList = [
 	{ id: "1", text: "выучить html", completed: true },
 	{ id: "2", text: "выучить css", completed: true },
@@ -29,20 +13,12 @@ const tasksList = [
 const todoList = document.querySelector('.todo-list');
 const listItemTemplate = document.querySelector('#list-item-template').content.querySelector('li');
 
-
->>>>>>> baa6e84... Merge
 listItemTemplate.querySelector('div').classList.add('view');
 listItemTemplate.querySelector('input').classList.add('toggle');
 listItemTemplate.querySelector('input').setAttribute('type', 'chekbox');
 listItemTemplate.querySelector('button').classList.add('destroy');
 
-<<<<<<< HEAD
-// Add information from Enter data to List item Templete 
 let createListItem = function (tasks) {
-=======
-
-let createListItem = function (task) {
->>>>>>> baa6e84... Merge
 
 	let listItem = listItemTemplate.cloneNode(true);
 
@@ -51,7 +27,6 @@ let createListItem = function (task) {
 	return listItem;
 };
 
-// Reder tasks using a List item Templete  
 const fragment = document.createDocumentFragment();
 
 let renderTasks = function () {
@@ -66,3 +41,30 @@ let renderTasks = function () {
 };
 
 renderTasks();
+
+let getId = function (idsDataArray) {
+
+	let currentTaskId = 0;
+
+	for (let i = 0; i < idsDataArray.length; i++) {
+		if (+idsDataArray[i].id > currentTaskId) {
+			currentTaskId = +idsDataArray[i].id;
+		}
+	}
+
+
+	return (currentTaskId + 1) + '';
+};
+
+let createNewTask = function () {
+
+	let newTask = {
+		id: getId(tasksList),
+		text: "выучить css",
+		completed: true
+	};
+	tasksList.push(newTask);
+	renderTasks();
+};
+
+createNewTask();
